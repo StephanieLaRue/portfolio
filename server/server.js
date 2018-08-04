e9b5b8f7f74bb9ecfe258415a83a34d7bf6c53ac
stephanie <stephlarue26@gmail.com>
2018-08-04 11:12:49 -0700

    added proxy for legwebsite and added project to index

--- server/server.js
+++ server/server.js
@@ -8,7 +8,6 @@ const fs = require('fs');
 const path = require('path');
 const mail = require('./mailer');
 const port = process.argv[2] || 80;
-const quiz = require('../projects/quiz-app/mongo/mongodb.js');
 const blog = require('./blog-mongo/mongoDB.js');
 const key = require('./blog-mongo/auth.js');
 
@@ -25,13 +24,17 @@ app.use('/api/quiz-app', proxy({
   target: 'http://localhost:3002'
 }));
 
+app.use('/api/portfolio.1.0', proxy({
+  target: 'http://localhost:3003'
+}));
+
 app.use(express.static(path.join(__dirname, '../public')))
 app.use('/fitnessapp.2.0', express.static(path.join(__dirname, '../../fitnessapp.2.0')))
 app.use('/calculatortwo', express.static(path.join(__dirname, '../../calculatortwo')))
 app.use('/fitnessapp', express.static(path.join(__dirname, '../../fitnessapp')))
 app.use('/quiz-app', express.static(path.join(__dirname, '../../quiz-app')))
+app.use('/portfolio.1.0', express.static(path.join(__dirname, '../../portfolio.1.0')))
 
-app.use('/projects', express.static(path.join(__dirname, '../projects')))
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
   extended: true
@@ -52,4 +55,4 @@ app.post('/newPost', blog.form)
 app.post('/key', key.matchKey)
 
 
-app.listen(port, () => console.log('Express server listening on port 80...'))
+app.listen(port, () => console.log(`Express server listening on ${port}...`))
